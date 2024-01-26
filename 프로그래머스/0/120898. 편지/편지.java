bbec66e3dfa4360317323cdd88326ef899d9d60d
Jeongin917 <150813239+Jeongin917@users.noreply.github.com>
2024-01-26 17:08:06 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String message) {
+
+        return message.length() * 2;
+        // String는 class이므로 length를 활용하기 위해서는 method를 사용해야하므로 length()로 작성필요
+    }
+}
