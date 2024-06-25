66f12c7e56ac209086b902228d5d4188fbe1d550
Jeongin917 <150813239+Jeongin917@users.noreply.github.com>
2024-06-25 21:18:29 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,6 @@
 -- 코드를 작성해주세요
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPER_INFOS
-where 'Python' in (SKILL_1, SKILL_2, SKILL_3) -- where in 함수를 사용하여 작성
+where 'Python' in (SKILL_1, SKILL_2, SKILL_3)
+-- where in 함수를 사용하여 작성
 order by ID;
